563044375ef16a11d3eab86b46f69fd33330a3cf
ryanmpdev <ryanmaciel697@gmail.com>
2022-07-19 22:56:12 -0300

    primeiro desafio de ingês

---
+++ script.js
@@ -0,0 +1,25 @@
+const comedy = [
+    {
+       title: "Os farofeiros", 
+       director:"Roberto Santucci",
+       year: 2017
+    }
+]
+
+const action = [
+    {
+        title: "Velozes e furiosos",
+        director: "Rob Cohen",
+        year: 2001
+    }
+]
+
+const fantasy = [
+    {
+        title: "Homem-Aranha: Sem Volta Para Casa",
+        director: "Jon Watts ",
+        year: 2021
+    }
+]
+
+
